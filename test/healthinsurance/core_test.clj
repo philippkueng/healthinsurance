43bf3ca559545910716772fcaeb3741b87f3a11d
Philipp Küng <git@philippkueng.ch>
2018-11-22 19:17:29 +0100

    remove files which are not necessary

--- test/healthinsurance/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns healthinsurance.core-test
-  (:require [clojure.test :refer :all]
-            [healthinsurance.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
