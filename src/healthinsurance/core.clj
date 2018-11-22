241a24a7fc574dc3a97d5c9b398d2401440cc9f0
Philipp Küng <git@philippkueng.ch>
2018-11-22 19:08:54 +0100

    remove code which is no longer needed

--- src/healthinsurance/core.clj
+++ src/healthinsurance/core.clj
@@ -15,18 +15,10 @@
              [1000 301.10]
              [1500 273.40]
              [2000 245.70]
-             [2500 218]]
-
-   :kid [[0 81]
-         [100 80]
-         [200 79]
-         [300 78]]})
+             [2500 218]]})
 
 (def healthcare-costs (range 100 2600 100))
 (def liabilities [300 500 1000 1500 2000 2500])
-
-;; have a function to write the datastructure to a CSV
-#_(def headers ["name and liability" "monthly payment" ...a range of costs...])
 
 (defn- calculate-yearly-payment [monthly-cost liability consultancy-cost]
   (let [yearly-base-cost (* 12 monthly-cost)]
@@ -34,11 +26,6 @@
          consultancy-cost
          (+ liability (* 0.1 (- consultancy-cost liability))))
       yearly-base-cost)))
-
-(comment
-  #_ (calculate-yearly-payment 357.7 300 200)
-  #_ (calculate-yearly-payment 357.7 300 400)
-  )
 
 (dorun
   (let [individuals (keys data)]
